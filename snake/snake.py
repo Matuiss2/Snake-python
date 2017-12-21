afef37148022902952f2304704feeec07fad239a
Mateus <31539494+Matuiss2@users.noreply.github.com>
2017-12-21 15:50:07 -0200

    Update snake.py

--- snake/snake.py
+++ snake/snake.py
@@ -1,15 +1,17 @@
 # V 1.0 - Functional game
 # V 1.06 - Improves the readability, improves slightly the performance
 # V 1.08 - Adds some sfx(eating and game over)
-# V 1.2 - Adds background music, removes the score on the top left(just showing it at the end)
+# V 1.2 - Adds background music, shows the score only in the end
 # V 1.23 - The music now loops
-# V 1.26 - Adds menu skeleton, changes the background color to light gray to avoid confusion with the taskbar
-# V 1.32 - Changed the variable name fps to speed to avoid confusion, few performance improvements(direction, sounds)
-# V 1.38 - Performance boost by changing most of the lists to deque, O(1) vs the previous O(n)
+# V 1.26 - Changes the background color to light gray to avoid confusion
+# V 1.32 - Few performance improvements(direction, sounds) and redability
+# V 1.38 - Changes most of the lists to deque, O(1) vs the previous O(n)
 # V 1.48 - BugFix, relative paths
-# V 1.57 - Adds pause/ unpausing (pressing p), code slightly more readable
+# V 1.59 - Adds pause(pressing p) and a pause menu, code more readable
+# V 1.62 - Most of flask8 recommended changes added
+# V 1.65 - Most of J_H (thanks for the code review) recommended changes added
 """
-todo Make a pause menu, Make a initial menu,add difficult settings,add another music options,add buttons, add tutorials
+todo add diffic settings ,add another musics, add tutorials in the beginning
 """
 import pygame
 import sys
@@ -21,16 +23,19 @@ import os
 
 
 def main():
-    """Snake v 1.57"""
+    """Snake v 1.68."""
+    font = "times new roman"
     score = 0  # Initial score
     speed = pygame.time.Clock()
     direction = "R"  # Initial direction
-    snake_position = collections.deque([100, 50])  # Initial snake position
-    snake_body = collections.deque([[100, 50], [90, 50], [100, 50]])  # Initial snake body
+    # starting position and body
+    snake_pos = collections.deque([100, 50])
+    snake_body = collections.deque([[100, 50], [90, 50], [100, 50]])
     # It places the food randomly, excluding the border
-    food_position = [random.randrange(1, 72) * 10, random.randrange(1, 46) * 10]
-    food_spawn = True
+    food_pos = (random.randrange(1, 72) * 10, random.randrange(1, 46) * 10)
+    food_spawned = True
     # Will define the colors
+    white = pygame.Color("white")
     red = pygame.Color("red")
     green = pygame.Color("green")
     black = pygame.Color("black")
@@ -46,16 +51,16 @@ def main():
         the mixers, and game """
         pygame.mixer.pre_init(44100, -16, 1, 512)
         pygame.mixer.init()
-        bugs = pygame.init()
-        if bugs[1] > 0:
-            print("There are", bugs[1], "bugs! quiting.....")
+        bugs = pygame.init()[1]
+        if bugs > 0:
+            print("There are", bugs, "bugs! quiting.....")
             time.sleep(3)
             sys.exit("Closing program")
         else:
             print("The game was initialized")
 
     def game_sound(s):
-        """ Include the game sfx and music"""
+        """ Include the game sfx and music."""
         if s == 0:
             directory = os.path.dirname(os.path.realpath(sys.argv[0]))
             full_path = os.path.join(directory, "background.ogg")
@@ -71,9 +76,8 @@ def main():
             pygame.mixer.Sound(full_path).play()
 
     def you_lose():
-        """ When the players loses, it will show a red message in times new
-         roman font with 44 px size in a rectangle box"""
-        font_game_over = pygame.font.SysFont("times new roman", 44)
+        """ Is called to show the game over message and the score."""
+        font_game_over = pygame.font.SysFont(font, 44)
         game_over_surface = font_game_over.render("Game over :(", True, red)
         game_over_position = game_over_surface.get_rect()
         game_over_position.midtop = (360, 15)
@@ -83,20 +87,29 @@ def main():
         pygame.display.flip()  # Updates the screen, so it doesnt freeze
         quiting()
 
+    def pause_menu():
+        """It displays the pause menu."""
+        player_screen.fill(white)
+        pause_font = pygame.font.SysFont(font, 44)
+        pause_surface = pause_font.render("Paused", True, black)
+        pause_position = pause_surface.get_rect()
+        pause_position.midtop = (360, 15)
+        player_screen.blit(pause_surface, pause_position)
+        pygame.display.flip()
+
     def quiting():
-        """ When this function is called, it will wait 4 seconds and exit"""
+        """ When this function is called, it will wait 4 seconds and exit."""
         time.sleep(4)
         pygame.quit()
         sys.exit()
 
     def scoring():
-        """ It will shows the score after the game over in times new
-        roman font with 16px size and black color in a rectangle box"""
-        score_font = pygame.font.SysFont("times new roman", 16)
-        score_surface = score_font.render("Score : {}".format(score), True, black)
-        score_position = score_surface.get_rect()
-        score_position.midtop = (360, 80)
-        player_screen.blit(score_surface, score_position)
+        """ It will show the score after the game over."""
+        scr_font = pygame.font.SysFont(font, 16)
+        scr_surface = scr_font.render("Score : {}".format(score), True, black)
+        scr_position = scr_surface.get_rect()
+        scr_position.midtop = (360, 80)
+        player_screen.blit(scr_surface, scr_position)
 
     initializer()
     game_sound(0)
@@ -110,6 +123,7 @@ def main():
                     paused = not paused
                     if paused:
                         pygame.mixer.music.pause()
+                        pause_menu()
                     else:
                         pygame.mixer.music.unpause()
                 # Choose direction by user input, block opposite directions
@@ -131,35 +145,35 @@ def main():
         # Simulates the snake movement(together with snake_body_pop)
         if not paused:
             if direction == "R":
-                snake_position[0] += 10
+                snake_pos[0] += 10
             elif direction == "L":
-                snake_position[0] -= 10
+                snake_pos[0] -= 10
             elif direction == "D":
-                snake_position[1] += 10
+                snake_pos[1] += 10
             elif direction == "U":
-                snake_position[1] -= 10
+                snake_pos[1] -= 10
             # Body mechanics
-            snake_body.appendleft(list(snake_position))
-            if snake_position == collections.deque(food_position):
+            snake_body.appendleft(list(snake_pos))
+            if snake_pos == collections.deque(food_pos):
                 score += 1  # Every food taken will raise the score by 1
                 game_sound(1)
-                food_spawn = False  # It removes the food from the board
+                food_spawned = False  # remove the food piece that was eaten
             else:
-                # If the food is taken it will not remove the last body piece(raising snakes size)
+                # If the food is taken it will not remove the last body piece
                 snake_body.pop()
-            if food_spawn is False:  # When a food is taken it will respawn randomly
-                food_position = [random.randrange(1, 72) * 10, random.randrange(1, 46) * 10]
-            food_spawn = True  # It will set the food to True again, to keep the cycle
+            if not food_spawned:  # When a food is taken it ll respawn randomly
+                food_pos = (random.randrange(1, 72) * 10, random.randrange(1, 46) * 10)
+            food_spawned = True    # Keep the food spawn cycle forever
             # Drawing
             player_screen.fill(grey)  # Set the background to grey
             for position in snake_body:  # Snake representation on the screen
                 pygame.draw.rect(player_screen, green, pygame.Rect(position[0], position[1], 10, 10))
             # Food representation on the screen
-            pygame.draw.rect(player_screen, orange, pygame.Rect(food_position[0], food_position[1], 10, 10))
-            if snake_position[0] not in range(0, 711) or snake_position[1] not in range(0, 451):
+            pygame.draw.rect(player_screen, orange, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
+            if snake_pos[0] not in range(0, 711) or snake_pos[1] not in range(0, 451):
                 you_lose()  # Game over when the Snake hit a wall
             for block in itertools.islice(snake_body, 1, None):
-                if snake_position == collections.deque(block):
+                if snake_pos == collections.deque(block):
                     you_lose()  # Game over when the Snake hits itself
             pygame.display.flip()  # It constantly updates the screen
             speed.tick(26)  # It sets the speed to a playable value
