29f242db0999df32307224683240f27240240ed3
Mateus <31539494+Matuiss2@users.noreply.github.com>
2018-01-31 20:12:44 -0200

    Update snake.py

--- snake/snake.py
+++ snake/snake.py
@@ -1,14 +1,15 @@
+# 0.01-super small changes to 0.1 - super big changes
 # V 1.0 - Functional game
 # V 1.06 - Improves the readability, improves slightly the performance
 # V 1.08 - Adds some sfx(eating and game over)
 # V 1.2 - Adds background music, shows the score only in the end
 # V 1.23 - The music now loops
 # V 1.26 - Changes the background color to light gray to avoid confusion
-# V 1.32 - Few performance improvements(direction, sounds) and redability
+# V 1.32 - Few performance and readability improvements(direction, sounds)
 # V 1.38 - Changes most of the lists to deque, O(1) vs the previous O(n)
 # V 1.48 - BugFix, relative paths
 # V 1.59 - Adds pause(pressing p) and a pause menu, code more readable
-# V 1.62 - Most of flask8 recommended changes added
+# V 1.62 - Most of flask8 recommended changes added, few permonce improvements
 # V 1.65 - Most of J_H (thanks for the code review) recommended changes added
 """
 todo add diffic settings ,add another musics, add tutorials in the beginning
@@ -25,6 +26,7 @@ import os
 def main():
     """Snake v 1.68."""
     font = "times new roman"
+    argument = sys.argv[0]
     score = 0  # Initial score
     speed = pygame.time.Clock()
     direction = "R"  # Initial direction
@@ -43,7 +45,7 @@ def main():
     grey = pygame.Color("light grey")
     # Game surface
     player_screen = pygame.display.set_mode((720, 460))  # Set screen size
-    pygame.display.set_caption("Snake v.1.38")  # Set screen title and version
+    pygame.display.set_caption("Snake v.1.68")  # Set screen title and version
 
     def initializer():
         """ Checks the mistakes, and closes the program if it does while
@@ -51,7 +53,7 @@ def main():
         the mixers, and game """
         pygame.mixer.pre_init(44100, -16, 1, 512)
         pygame.mixer.init()
-        bugs = pygame.init()[1]
+        bugs = pygame.init()[1]  # (passed, failed)
         if bugs > 0:
             print("There are", bugs, "bugs! quiting.....")
             time.sleep(3)
@@ -62,16 +64,16 @@ def main():
     def game_sound(s):
         """ Include the game sfx and music."""
         if s == 0:
-            directory = os.path.dirname(os.path.realpath(sys.argv[0]))
+            directory = os.path.dirname(os.path.realpath(argument))
             full_path = os.path.join(directory, "background.ogg")
             pygame.mixer.music.load(full_path)
             pygame.mixer.music.play(-1)
         elif s == 1:
-            directory = os.path.dirname(os.path.realpath(sys.argv[0]))
+            directory = os.path.dirname(os.path.realpath(argument))
             full_path = os.path.join(directory, "eating.wav")
             pygame.mixer.Sound(full_path).play()
         elif s == 2:
-            directory = os.path.dirname(os.path.realpath(sys.argv[0]))
+            directory = os.path.dirname(os.path.realpath(argument))
             full_path = os.path.join(directory, "game-over.wav")
             pygame.mixer.Sound(full_path).play()
 
@@ -157,20 +159,34 @@ def main():
             if snake_pos == collections.deque(food_pos):
                 score += 1  # Every food taken will raise the score by 1
                 game_sound(1)
-                food_spawned = False  # remove the food piece that was eaten
+                food_spawned = False  # Remove the food piece that was eaten
             else:
                 # If the food is taken it will not remove the last body piece
                 snake_body.pop()
             if not food_spawned:  # When a food is taken it ll respawn randomly
-                food_pos = (random.randrange(1, 72) * 10, random.randrange(1, 46) * 10)
-            food_spawned = True    # Keep the food spawn cycle forever
+                food_pos = (
+                    random.randrange(
+                        1,
+                        72) * 10,
+                    random.randrange(
+                        1,
+                        46) * 10)
+            food_spawned = True    # Keep the food spawn cycle going
             # Drawing
             player_screen.fill(grey)  # Set the background to grey
             for position in snake_body:  # Snake representation on the screen
-                pygame.draw.rect(player_screen, green, pygame.Rect(position[0], position[1], 10, 10))
+                pygame.draw.rect(
+                    player_screen, green, pygame.Rect(
+                        position[0], position[1], 10, 10))
             # Food representation on the screen
-            pygame.draw.rect(player_screen, orange, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
-            if snake_pos[0] not in range(0, 711) or snake_pos[1] not in range(0, 451):
+            pygame.draw.rect(
+                player_screen, orange, pygame.Rect(
+                    food_pos[0], food_pos[1], 10, 10))
+            if snake_pos[0] not in range(
+                    0,
+                    711) or snake_pos[1] not in range(
+                    0,
+                    451):
                 you_lose()  # Game over when the Snake hit a wall
             for block in itertools.islice(snake_body, 1, None):
                 if snake_pos == collections.deque(block):
